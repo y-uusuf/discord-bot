67105c1f31650a4d20fceff89f5de6c4d4335dfb
yusuf. <frxbid@gmail.com>
2025-08-02 05:19:47 +0100

    .

--- commands/reload.js
+++ commands/reload.js
@@ -8,22 +8,25 @@ module.exports = {
       return message.reply("*sorry, you don't have permission to reload commands.*");
     }
 
-    const commandName = args[0];
-    if (!commandName) return message.reply("*please, specify a command to reload.*");
+    const commandName = args[0].toLowerCase();
+    const commandPath = path.join(__dirname, `${commandName}.js`);
 
-    const commandPath = path.join(__dirname, '..', `${commandName}.js`);
     if (!fs.existsSync(commandPath)) {
-      return message.reply(`*command \`${commandName}\` does not exist.*`);
+      return message.reply(`*the command \`${commandName}\` does not exist.*`);
     }
 
     try {
+      // Remove the cached version of the command
       delete require.cache[require.resolve(commandPath)];
+
+      // Reload the command
       const newCommand = require(commandPath);
       client.commands.set(newCommand.name, newCommand);
-      message.reply(`\`${commandName}\` has been reloaded.`);
-    } catch (err) {
-      console.error(err);
-      message.reply(`*sorry, i couldn't reload \`${commandName}\`:*\n \`${err.message}\``);
+
+      message.reply(`*the command \`${commandName}\` was successfully reloaded.*`);
+    } catch (error) {
+      console.error(error);
+      message.reply(`*sorry, i couldn't reload \`${commandName}\`.*`);
     }
   },
 };
