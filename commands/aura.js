50d8a90821b05188bfa1881c77185b224c2b19ad
yusuf. <frxbid@gmail.com>
2025-08-02 14:12:49 +0100

    .

--- commands/aura.js
+++ commands/aura.js
@@ -4,13 +4,23 @@ const Level = require("../models/level"); // adjust path as needed
 module.exports = {
   name: "aura",
   async execute(client, message) {
-    const userID = message.author.id;
+
+     let target =
+      message.mentions.users.first() ||
+      (args[0] && (await client.users.fetch(args[0]).catch(() => null))) ||
+      message.author;
+    
+    const userID = target.id;
     const guildID = message.guild.id;
 
     let userData = await Level.findOne({ userID, guildID });
 
-    if (!userData) {
-      return message.reply("*you don't have any aura yet. start chatting to collect it.*");
+     if (!userData) {
+        return message.reply(
+          target.id === message.author.id
+           ? "*you don't have any aura yet. start chatting to get it.*"
+            : `*${target.username} doesn't have any aura yet.*`
+      );
     }
 
     const embed = new MessageEmbed()
@@ -22,7 +32,7 @@ module.exports = {
         { name: "```aura```", value: `\`${userData.aura.toLocaleString()}.\``, inline: true },
         { name: "```messages```", value: `\`${userData.messages} / ${15 * userData.level}.\``, inline: true }
       )
-      .setFooter({ text: `requested by ${message.author.tag}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
+      .setFooter({ text: `requested by ${target.tag}`, iconURL: target.displayAvatarURL({ dynamic: true }) })
       .setTimestamp();
 
     message.channel.send({ embeds: [embed] });
