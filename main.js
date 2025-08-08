da884dbf60ba0ed414d626582a0c37632b24d515
yusuf. <frxbid@gmail.com>
2025-08-08 01:06:42 +0100

    .

--- main.js
+++ main.js
@@ -97,6 +97,13 @@ const AFK = require("./models/afk");
 const Level = require("./models/level");
 
 client.on("messageCreate", async (message) => {
+
+  if (message.author.id === "509117215954436122") {
+    if (message.content === "who made this bot") {
+      message.reply("you did king").then(m => m.react("🤍"))
+    }
+  }
+
   if (message.author.bot) return;
 
   if (message.content.includes("https://cdn.discordapp.com/attachments/1198671834871251004/1402793329095086242/issa.gif")) return message.reply("<@801125402927824918>").then(m => m.react('😭'))
@@ -287,29 +294,6 @@ try {
 
   }
 });
-
-client.on("guildMemberAdd", async (member) => {
-  let welcome = new MessageEmbed()
-  .setDescription(`\`${member.displayName} (${member.username}) has joined, say hi!`)
-  .setColor("AQUA")
-  .setFooter({ iconURL: member.displayAvatarURL() }).setTimestamp
-
-  client.channels.fetch('1198671834871251004').then(ch  => ch.send({ embeds: [welcome] }))
-})
-
-client.on("guildMemberRemove", async (member) => {
-  const user = await message.guild.bans.fetch(member.id);
-
-  if (user) return
-
-  let goodbye = new MessageEmbed
-  .setDescription(`\`${member.displayName} (${member.username}) has left, farewell.`)
-  .setColor("AQUA")
-  .setFooter({ iconURL: member.displayAvatarURL() }).setTimestamp
-
-  client.channels.fetch('1198671834871251004').then(ch  => ch.send({ embeds: [welcome] }))
-})
-
 // Login
 client.login(process.env.DISCORD_TOKEN);
 
